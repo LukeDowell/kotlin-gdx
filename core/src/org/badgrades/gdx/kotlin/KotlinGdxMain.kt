d4454cb4f27b93aec949204fbb5a62f03c2ba944
Luke Dowell <lukedowell@gmail.com>
2016-08-02 14:58:57 -0500

    Spinny plane

--- core/src/org/badgrades/gdx/kotlin/KotlinGdxMain.kt
+++ core/src/org/badgrades/gdx/kotlin/KotlinGdxMain.kt
@@ -3,28 +3,53 @@ package org.badgrades.gdx.kotlin
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
-import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.utils.Timer
+import com.badlogic.gdx.utils.Timer.Task
 
 class KotlinGdxMain : ApplicationAdapter() {
+
     lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+    lateinit var textureAtlas: TextureAtlas
+    lateinit var sprite: Sprite
+
+    var currentFrame: Int = 1
+    var currentAtlasKey: String = "0001"
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        textureAtlas = TextureAtlas(Gdx.files.internal("spritesheet.atlas"))
+        val region: TextureAtlas.AtlasRegion? = textureAtlas.findRegion(currentAtlasKey)
+
+        sprite = Sprite(region)
+        sprite.setPosition(120f, 100f)
+        sprite.scale(2.5f)
+
+        Timer.schedule(object: Task() {
+            override fun run() {
+                currentFrame++
+                if(currentFrame > 20)
+                    currentFrame = 1
+
+                currentAtlasKey = String.format("%04d", currentFrame)
+                sprite.setRegion(textureAtlas.findRegion(currentAtlasKey))
+            }
+        }, 0f, 1/30.0f)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        sprite.draw(batch)
         batch.end()
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        textureAtlas.dispose()
     }
 }
