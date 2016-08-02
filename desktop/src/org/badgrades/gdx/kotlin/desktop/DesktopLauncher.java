084a1ba631474beb4e9c52c56a4201d8af283c92
Luke Dowell <lukedowell@gmail.com>
2016-08-02 09:43:06 -0500

    Initial commit

---
+++ desktop/src/org/badgrades/gdx/kotlin/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.badgrades.gdx.kotlin.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.badgrades.gdx.kotlin.KotlinGdxMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new KotlinGdxMain(), config);
+	}
+}
