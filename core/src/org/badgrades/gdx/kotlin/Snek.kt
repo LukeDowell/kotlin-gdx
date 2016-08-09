a9fd6a68c67d425f3d9b8886500e41aae79f5ef3
Luke Dowell <ldowell@open-137.mikeshouse.bravo>
2016-08-09 15:45:29 -0500

    Snek movin around

--- core/src/org/badgrades/gdx/kotlin/Snek.kt
+++ core/src/org/badgrades/gdx/kotlin/Snek.kt
@@ -4,34 +4,94 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import java.awt.Point
 import java.util.*
 
-class Snek : ApplicationAdapter() {
+class SnekGame : ApplicationAdapter() {
 
     val random = Random()
+
+    /** The width of the map in blocks */
     val MAP_WIDTH = 20
+
+    /** The height of the map in blocks */
     val MAP_HEIGHT = 20
 
+    /** The size of a single block in pixels */
+    val BLOCK_SIZE = 16
+
+    val camera = OrthographicCamera()
+
     val foods: MutableCollection<Food> = mutableListOf(createRandomFood())
-    val snake: Snake = Snake(MAP_WIDTH / 2, MAP_HEIGHT / 2)
+    val snek: Snek = Snek(MAP_WIDTH / 2, MAP_HEIGHT / 2)
+
+    lateinit var shapeRenderer: ShapeRenderer
 
     override fun create() {
+        camera.setToOrtho(false,
+                (MAP_HEIGHT * BLOCK_SIZE).toFloat(),
+                (MAP_WIDTH * BLOCK_SIZE).toFloat())
+        shapeRenderer = ShapeRenderer()
     }
 
     override fun render() {
         Gdx.gl.glClearColor(0f, 0f, 0f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) snake.direction = Direction.DOWN
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) snake.direction = Direction.UP
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) snake.direction = Direction.LEFT
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) snake.direction = Direction.RIGHT
+        // Logic
+        update()
 
+        // Camera and Shape renderer
+        camera.update()
+        shapeRenderer.projectionMatrix = camera.combined
 
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+
+        // Food
+        shapeRenderer.setColor(0f, 1f, 0f, 1f)
+        for(food in foods) {
+            shapeRenderer.rect(
+                    food.x.toFloat(),
+                    food.y.toFloat(),
+                    BLOCK_SIZE.toFloat(),
+                    BLOCK_SIZE.toFloat())
+        }
+
+        // Draw head of snek
+        shapeRenderer.setColor(1f, 0f, 0f, 1f)
+        shapeRenderer.rect(
+                snek.x.toFloat(),
+                snek.y.toFloat(),
+                BLOCK_SIZE.toFloat(),
+                BLOCK_SIZE.toFloat())
+
+        // Draw body of snek
+        shapeRenderer.setColor(0f, 0f, 1f, 1f)
+        for(cell in snek.cells) {
+            shapeRenderer.rect(
+                    cell.x.toFloat(),
+                    cell.y.toFloat(),
+                    BLOCK_SIZE.toFloat(),
+                    BLOCK_SIZE.toFloat())
+        }
+
+        shapeRenderer.end()
     }
 
     override fun dispose() {
+    }
+
+    fun update() {
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) snek.direction = Direction.DOWN
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) snek.direction = Direction.UP
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) snek.direction = Direction.LEFT
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) snek.direction = Direction.RIGHT
+
+        snek.move()
+
+        // TODO check for collisions: food, wall, self
     }
 
     fun createRandomFood(): Food = Food(random.nextInt(MAP_WIDTH), random.nextInt(MAP_HEIGHT))
@@ -44,7 +104,7 @@ class Snek : ApplicationAdapter() {
  * <_____________________________/   \
  *
  */
-class Snake(x: Int, y: Int) {
+class Snek(var x: Int, var y: Int) {
     val cells = mutableListOf(Point(x, y))
     var direction = Direction.RIGHT
 
@@ -52,6 +112,35 @@ class Snake(x: Int, y: Int) {
         val butt = cells.last()
         for(i in 1.rangeTo(length))
             cells.add(Point(butt.x, butt.y))
+    }
+
+    fun move() {
+
+        // Store the original position of the head
+        val oldHeadX = x
+        val oldHeadY = y
+
+        // Move the head of the snek
+        when(direction) {
+            Direction.LEFT -> x--
+            Direction.RIGHT -> x++
+            Direction.DOWN -> y--
+            else -> y++
+        }
+
+        // Move the butts
+        // Each cell will move to the spot occupied by the cell ahead of it
+        for(i in 0.rangeTo(cells.size - 1)) {
+
+            if(i == 0) {
+                // If it's the first cell, move it to where the head used to be
+                cells[i].x = oldHeadX
+                cells[i].y = oldHeadY
+            } else {
+                cells[i].x = cells[i - 1].x
+                cells[i].y = cells[i - 1].y
+            }
+        }
     }
 }
 
--- desktop/src/org/badgrades/gdx/kotlin/desktop/DesktopLauncher.kt
+++ desktop/src/org/badgrades/gdx/kotlin/desktop/DesktopLauncher.kt
@@ -2,11 +2,11 @@ package org.badgrades.gdx.kotlin.desktop
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import org.badgrades.gdx.kotlin.Snek
+import org.badgrades.gdx.kotlin.SnekGame
 
 object DesktopLauncher {
     @JvmStatic fun main(arg: Array<String>) {
         val config = LwjglApplicationConfiguration()
-        LwjglApplication(Snek(), config)
+        LwjglApplication(SnekGame(), config)
     }
 }
